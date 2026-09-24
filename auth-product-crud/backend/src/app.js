e34cd7fd06dc30dd88a5bd1eaee7d80eecafea93
Aditya3488 <adityatiwari062@gmail.com>
2026-09-24 12:04:40 +0530

    Use CLIENT_URL env variable in CORS config

--- auth-product-crud/backend/src/app.js
+++ auth-product-crud/backend/src/app.js
@@ -5,8 +5,8 @@ const cors = require("cors");
 const app = express()
 
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true,
 }))
 app.use(express.json())
 app.use(cookiesParser())
